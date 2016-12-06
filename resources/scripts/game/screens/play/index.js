7e5a838d2d73651815be7d32acd43ccfe9a34129
DAB0mB <emanor6@gmail.com>
2016-12-06 02:21:30 -0200

    Back to menu on esc press

--- resources/scripts/game/screens/play/index.js
+++ resources/scripts/game/screens/play/index.js
@@ -1,5 +1,18 @@
 Game.Screens.Play = class Play extends Engine.Screen {
+  get events() {
+    return {
+      "keydown": "onKeyDown"
+    }
+  }
+
   initialize() {
     this.appendLayer(Game.Screens.Play.Ready);
+  }
+
+  onKeyDown() {
+    // esc
+    if (this.keyStates.get(27)) {
+      this.game.changeScreen(Game.Screens.Menu);
+    }
   }
 };
